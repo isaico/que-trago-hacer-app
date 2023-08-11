b25b8bc9ee2bf5e16e8288542d89351125441048
isaias garcia <isaico@outlook.com>
2023-08-11 15:08:45 -0300

    adx.txt agregado a rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Layout from './layout/Layout';
 import HowToUse from './layout/HowToUse';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ads from "../public/ads.txt"
 function App() {
     return (
         <>
@@ -9,6 +10,7 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Layout />} />
                     <Route path="/how-to-use" element={<HowToUse />} />
+                    <Route path="/ads.txt" element={ads} />
                 </Routes>
             </BrowserRouter>
         </>
